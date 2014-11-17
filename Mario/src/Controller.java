ad6363150cede46bacd3468e7b40f3ee760998a8
ros_mcvanderwilt <ros_mcvanderwilt@WROSRM31922.seattleschools.org>
2014-11-17 08:45:07 -0800

    11.17; introduced controller class

---
+++ Mario/src/Controller.java
@@ -0,0 +1,43 @@
+import java.awt.event.KeyEvent;
+
+
+public class Controller {
+	
+	public int MarioXPos;
+	public int MarioYPos;
+	
+	private boolean leftKeyPressed;
+	private boolean rightKeyPressed;
+	private boolean upKeyPressed;
+	
+	public Controller(){
+		
+	}
+	
+	private void processKeyPress (KeyEvent e){
+		int keyCode = e.getKeyCode();
+		if (keyCode == KeyEvent.VK_LEFT){
+			leftKeyPressed = true;
+			//Level drawn changes
+			//Mario Position changes
+			//Sprite position changes
+		} else if (keyCode == KeyEvent.VK_RIGHT){
+			rightKeyPressed = true;
+		} else if (keyCode == KeyEvent.VK_UP){
+			upKeyPressed = true;
+		}
+	}
+	//TODO: Account for multiple keys pressed
+}
+
+
+//process key press
+//updateLevel
+	//what section of array is drawn
+	//position of scrolling background
+//updateSprites
+	//COLLISION DETECTION
+	//way Mario is facing
+	//positions of other sprites (depending on Mario's movement)
+//drawLevel
+//drawSprites (or part of drawLevel)
--- Mario/src/Level.java
+++ Mario/src/Level.java
@@ -38,8 +38,9 @@ public class Level {
 		
 		frame = new JFrame();
 		frame.add(panel);
-		//frame.setResizable(false);
+		frame.setResizable(false);
 		frame.setSize(ARR_WIDTH * 32, ARR_HEIGHT * 32);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
 		tileSet = null;
@@ -66,9 +67,7 @@ public class Level {
 			for (int row = 0; row < ARR_HEIGHT; row++){
 				for (int column = 0; column < ARR_WIDTH; column++){
 					layout[column][row] = scanFile.nextInt();
-					//System.out.print(layout[column][row]);
 				}
-				//System.out.println();
 			}
 		}
 		return layout;
@@ -76,24 +75,23 @@ public class Level {
 	
 	public void drawLevel() throws IOException{
 		//Should only select area of area showing on panel
+		//Get Mario's position on screen
 		int xPos, yPos;
 		
 		Graphics g = panel.getGraphics();
 		//TODO: Shouldn't draw full image just a selection based on Mario's xPos
-		Image bckgrd = ImageIO.read(new File("background.png"));//TODO: Needs to be scaled
+		Image bckgrd = ImageIO.read(new File("background.png"));
 		Image scaledBkgrd = bckgrd.getScaledInstance(ARR_WIDTH * 32, ARR_HEIGHT * 32, java.awt.Image.SCALE_SMOOTH);
 		g.drawImage(scaledBkgrd, 0, 0, panel);
 		
-		for (int row = 0; row < ARR_HEIGHT; row++){
-			for (int column = 0; column < ARR_WIDTH; column++){
+		for (int row = 0; row < ARR_HEIGHT; row++){//Stays unchanging
+			for (int column = 0; column < ARR_WIDTH; column++){//TODO: Should alter starting point depending on Mario's position
 				
 				xPos = column * 32;
 				yPos = row * 32;
 				
 				int tileType = layout[column][row];
-				if (tileType != 0){//TODO: Shouldn't be necessary if tiles include transparency
-					g.drawImage(tiles[tileType], xPos, yPos, panel);
-				}
+				g.drawImage(tiles[tileType], xPos, yPos, panel);
 			}
 		}
 	}
