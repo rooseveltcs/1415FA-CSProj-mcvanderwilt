6b7893f6439e9bab5dc9e7a703a9d84980a4969f
Mia Vanderwilt <miavanderwilt@gmail.com>
2014-11-26 22:02:06 -0800

    Scrolling achieved, though not continuous. Sprites and brick layout diabled for the moment.

--- Mario/src/Level.java
+++ Mario/src/Level.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -110,13 +111,13 @@ public class Level {
 		int panelXPos, panelYPos;
 		
 		Graphics g = panel.getGraphics();//TODO: Make an instance field
-		
+		/*
 		//TODO: Shouldn't draw full image just a selection based on Mario's xPos
 		BufferedImage bckgrd = ImageIO.read(new File("background.png"));//TODO: Alter image to extend beyond panel width height
 		//Image scaledBkgrd = bckgrd.getScaledInstance(ARR_WIDTH * 32, (ARR_HEIGHT - 1) * 32, java.awt.Image.SCALE_SMOOTH);
 		g.drawImage(bckgrd, 0, 0, PXLS_PER_TILE * ARR_WIDTH, (ARR_HEIGHT - 1) * PXLS_PER_TILE, panel);
-		
-		int delta = 10;//for now//TODO: Test
+		*/
+		int delta = 0;//for now//TODO: Test
 		
 		for (int row = 0; row < ARR_HEIGHT; row++){//Stays unchanging
 			for (int column = 0; column < ARR_WIDTH; column++){
@@ -129,7 +130,8 @@ public class Level {
 				g.drawImage(tiles[tileType], panelXPos, panelYPos, panel);
 			}
 		}
-		drawSprites(g);//TODO: Don't really want this in here.
+		drawSprites(g);//TODO: Don't really want this in here. 
+		//panel.paintComponent(g);
 	}
 	
 	//Should go through array of active sprites in the level (initialized in constructor) and draw each of them
@@ -153,19 +155,22 @@ public class Level {
 			
 			if (keyCode == KeyEvent.VK_LEFT){
 				leftKeyPressed = true;
-				xPos+= 1;
+				panel.setXPos(-10);
 			} else if (keyCode == KeyEvent.VK_RIGHT){
 				rightKeyPressed = true;
+				panel.setXPos(10);
 			} else if (keyCode == KeyEvent.VK_UP){
 				upKeyPressed = true;
+				System.out.println(panel.topLeftXPos);
+				System.out.println(panel.topLeftXPos);
 			}
-			try {
-				updateLevel();
-				updateSprites();//goes in and adds delta to sprite's position; tests for collision; changes mario's state
+		/*	try {
+				//updateLevel();
+				////updateSprites();//goes in and adds delta to sprite's position; tests for collision; changes mario's state
 				drawLevel();
 			} catch (IOException e) {
 				System.out.println("Error: drawing level");
-			}
+			}*/
 		}
 
 		@Override
@@ -206,20 +211,37 @@ public class Level {
 			
 		}
 		
-		public void paintComponent(){
-			
-		}
-		
-		
 		@Override
-		public void repaint(){
-			//paint according to xPos
-			g.drawImage(bkgrdImg, 0, 0, PXLS_PER_TILE * ARR_WIDTH, (ARR_HEIGHT - 1) * PXLS_PER_TILE, panel);
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+
+			int breakPt = panel.getWidth() - topLeftXPos;//Point where one image stops and the other begins (tail and head meet)
+			//If mario is moving right the breakpoint should get smaller
+			
+			//Scale image
+			//TODO: Resolution's not great
+			int imgWidth = panel.getWidth();//Desired width/height of new scaled image
+			int imgHeight = panel.getHeight();
+			BufferedImage resizedImage = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_ARGB);
+			Graphics2D graphics = resizedImage.createGraphics();
+			graphics.drawImage(bkgrdImg, 0, 0, imgWidth, imgHeight, null);
+			graphics.dispose();
+			
+			//TODO: CLEAN UP
+			
+			//TODO: Scrolling isn't continuous
+			//image, panel beginning x pos, panel begin. y pos, panel end x pos, panel end y pos, img begin x pos, img begin y pos, img end x pos, img end y pos, panel
+			g.drawImage(resizedImage, 0, 0, panel.getWidth() - breakPt, panel.getHeight(), breakPt, 0, imgWidth, imgHeight, panel);
+			g.drawImage(resizedImage, panel.getWidth() - breakPt, 0, panel.getWidth(), panel.getHeight(), 0, 0, breakPt, imgHeight, panel);
+			
+			//g.drawImage(resizedImage, 0, 0, breakXPoint, panel.getHeight(), topLeftXPos, 0, bkgrdImg.getWidth(), bkgrdImg.getHeight(), panel);
+			//g.drawImage(resizedImage, breakXPoint, 0, panel.getWidth(), panel.getHeight(), 0, 0, bkgrdImg.getWidth() - breakXPoint, bkgrdImg.getHeight(), panel);
 			
 		}
 		
 		public void setXPos(int delta){
 			topLeftXPos += delta;
+			repaint();
 		}
 	}
 }
